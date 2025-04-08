b44dbfbec6f9c4c107fa1b14b219d16ca824120c
ArchitAvd <archit.avadhani@gmail.com>
2025-04-08 15:59:00 +0530

    fixed tab name bug

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabsLayout() {
         }}
       />
       <Tabs.Screen
-        name="account"
+        name="accounts"
         options={{
           title: "Account",
           tabBarIcon: ({ color, size }) => (
